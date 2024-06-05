bd23b847b6ba14bce8689c8de546deffc62a72d3
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-05 13:55:10 -0300

    feat: Add methods in PlaylistRepository
    - Update PlaylistRepositoryImpl to reflect changes

--- lib/data/repositories/playlist_repository_impl.dart
+++ lib/data/repositories/playlist_repository_impl.dart
@@ -2,8 +2,20 @@ import 'package:lua/domain/entities/entities.dart';
 import 'package:lua/domain/repositories/repositories.dart';
 
 class PlaylistRepositoryImpl implements PlaylistRepository {
+  final List<Playlist> _playlists = [];
+
   @override
   Future<void> createPlaylist(Playlist playlist) async {
-    // Lógica para salvar a playlist
+    _playlists.add(playlist);
+  }
+
+  @override
+  Future<List<Playlist>> getAllPlaylists() async {
+    return _playlists;
+  }
+
+  @override
+  Future<void> removePlaylist(int id) async {
+    _playlists.removeWhere((playlist) => playlist.id == id);
   }
 }
--- lib/domain/repositories/playlist_repository.dart
+++ lib/domain/repositories/playlist_repository.dart
@@ -2,4 +2,6 @@ import 'package:lua/domain/entities/entities.dart';
 
 abstract class PlaylistRepository {
   Future<void> createPlaylist(Playlist playlist);
+  Future<List<Playlist>> getAllPlaylists();
+  Future<void> removePlaylist(int id);
 }
