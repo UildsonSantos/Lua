d2d183697d36a49b5a0a3633d29ad2332c6bed8a
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-05 12:18:06 -0300

    feat: Add support for retrieving music file metadata
    - Use flutter_media_metadata to retrieve song metadata
    - Update MusicFileDataSource to populate SongModel with metadata

--- lib/data/sources/music_file_data_source.dart
+++ lib/data/sources/music_file_data_source.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter_media_metadata/flutter_media_metadata.dart';
 import 'package:lua/data/models/models.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -9,18 +12,25 @@ class MusicFileDataSource {
           .listSync()
           .where((file) => file.path.endsWith('.mp3'))
           .toList();
-      return musicFiles.map((file) {
-        return SongModel(
+
+      List<SongModel> songs = [];
+
+      for (var file in musicFiles) {
+        final metadata = await MetadataRetriever.fromFile(File(file.path));
+
+        songs.add(SongModel(
           id: file.uri.toString(),
-          title: file.uri.pathSegments.last,
+          title: metadata.trackName ?? file.uri.pathSegments.last,
+          artist: metadata.trackArtistNames?.join(', ') ?? 'Unknown Artist',
+          album: metadata.albumName ?? 'Unknown Album',
+          duration: metadata.trackDuration ?? 0,
           filePath: file.path,
-          artist: '',
-          album: '',
-          duration: 0,
-        );
-      }).toList();
+        ));
+      }
+
+      return songs;
     } else {
-      throw Exception('Diretório não encontrado');
+      throw Exception('Directory not found');
     }
   }
 }
