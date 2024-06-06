db51a3cadc942a2d9c69f8ee3bb9585c60edb826
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-06 12:06:14 -0300

    feat: Implement CRUD methods for songs and playlists

--- lib/data/datasource/db/database_helper.dart
+++ lib/data/datasource/db/database_helper.dart
@@ -1,3 +1,4 @@
+import 'package:lua/data/models/models.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -54,5 +55,91 @@ class DatabaseHelper {
         PRIMARY KEY (playlistId, songId)
       )
     ''');
+  }
+
+  Future<void> insertSong(SongModel song) async {
+    final db = await database;
+    await db.insert('songs', song.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> deleteSong(int id) async {
+    final db = await database;
+    await db.delete('songs', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<List<SongModel>> getAllSongs() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('songs');
+    return List.generate(maps.length, (i) {
+      return SongModel.fromMap(maps[i]);
+    });
+  }
+
+  Future<List<PlaylistModel>> getAllPlaylists() async {
+    final db = await database;
+    final List<Map<String, dynamic>> playlistMaps = await db.query('playlists');
+
+    List<PlaylistModel> playlists = [];
+
+    for (Map<String, dynamic> playlistMap in playlistMaps) {
+      List<Map<String, dynamic>> songMaps = await db.rawQuery('''
+        SELECT s.*
+        FROM songs s
+        INNER JOIN playlist_songs ps ON s.id = ps.songId
+        WHERE ps.playlistId = ?
+      ''', [playlistMap['id']]);
+
+      List<SongModel> songs =
+          songMaps.map((songMap) => SongModel.fromMap(songMap)).toList();
+
+      playlists.add(PlaylistModel(
+        id: playlistMap['id'],
+        name: playlistMap['name'],
+        songs: songs,
+      ));
+    }
+
+    return playlists;
+  }
+
+  Future<void> insertPlaylist(PlaylistModel playlist) async {
+    final db = await database;
+    int playlistId = await db.insert('playlists', playlist.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+
+    // Converter a lista de Song para SongModel
+    List<SongModel> songModels =
+        playlist.songs.map((song) => SongModel.fromSong(song)).toList();
+
+    for (SongModel song in songModels) {
+      await db.insert(
+          'playlist_songs', {'playlistId': playlistId, 'songId': song.id},
+          conflictAlgorithm: ConflictAlgorithm.replace);
+    }
+  }
+
+  Future<void> updatePlaylist(PlaylistModel playlist) async {
+    final db = await database;
+    await db.update('playlists', playlist.toMap(),
+        where: 'id = ?', whereArgs: [playlist.id]);
+    await db.delete('playlist_songs',
+        where: 'playlistId = ?', whereArgs: [playlist.id]);
+
+    // Converter a lista de Song para SongModel
+    List<SongModel> songModels =
+        playlist.songs.map((song) => SongModel.fromSong(song)).toList();
+
+    for (SongModel song in songModels) {
+      await db.insert(
+          'playlist_songs', {'playlistId': playlist.id, 'songId': song.id},
+          conflictAlgorithm: ConflictAlgorithm.replace);
+    }
+  }
+
+  Future<void> deletePlaylist(int id) async {
+    final db = await database;
+    await db.delete('playlists', where: 'id = ?', whereArgs: [id]);
+    await db.delete('playlist_songs', where: 'playlistId = ?', whereArgs: [id]);
   }
 }
