09270a91624bef70e71e920e73a3fd351753ff07
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-16 19:07:59 -0300

    feat: Add playlist navigation and control buttons  in PlayerWidget

--- lib/presentation/widgets/player_widget.dart
+++ lib/presentation/widgets/player_widget.dart
@@ -3,10 +3,51 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lua/domain/entities/entities.dart';
 import 'package:lua/presentation/blocs/blocs.dart';
 
-class PlayerWidget extends StatelessWidget {
+class PlayerWidget extends StatefulWidget {
   final Playlist playlist;
+  final int initialIndex;
 
-  const PlayerWidget({super.key, required this.playlist});
+  const PlayerWidget({
+    super.key,
+    required this.playlist,
+    this.initialIndex = 0,
+  });
+
+  @override
+  PlayerWidgetState createState() => PlayerWidgetState();
+}
+
+class PlayerWidgetState extends State<PlayerWidget> {
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = widget.initialIndex;
+    context
+        .read<SongBloc>()
+        .add(PlaySongEvent(widget.playlist.songs[currentIndex]));
+  }
+
+  void _playSong(int index) {
+    final song = widget.playlist.songs[index];
+    context.read<SongBloc>().add(PlaySongEvent(song));
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  void _playNextSong() {
+    if (currentIndex < widget.playlist.songs.length - 1) {
+      _playSong(currentIndex + 1);
+    }
+  }
+
+  void _playPreviousSong() {
+    if (currentIndex > 0) {
+      _playSong(currentIndex - 1);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +55,15 @@ class PlayerWidget extends StatelessWidget {
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
-            title: Text(playlist.name),
+            title: Text(widget.playlist.name),
           ),
           body: Column(
             children: [
               Expanded(
                 child: ListView.builder(
-                  itemCount: playlist.songs.length,
+                  itemCount: widget.playlist.songs.length,
                   itemBuilder: (context, index) {
-                    final song = playlist.songs[index];
+                    final song = widget.playlist.songs[index];
                     return ListTile(
                       title: Text(song.title),
                       subtitle: Text(song.artist),
@@ -31,8 +72,12 @@ class PlayerWidget extends StatelessWidget {
                 ),
               ),
               Row(
-                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  IconButton(
+                    icon: const Icon(Icons.skip_previous),
+                    onPressed: _playPreviousSong,
+                  ),
                   IconButton(
                     icon: Icon(
                       state is SongPlaying ? Icons.pause : Icons.play_arrow,
@@ -40,12 +85,14 @@ class PlayerWidget extends StatelessWidget {
                     onPressed: () {
                       if (state is SongPlaying) {
                         context.read<SongBloc>().add(PauseSongEvent());
-                      } else if (playlist.songs.isNotEmpty) {
-                        context
-                            .read<SongBloc>()
-                            .add(PlaySongEvent(playlist.songs[0]));
+                      } else if (widget.playlist.songs.isNotEmpty) {
+                        _playSong(currentIndex);
                       }
                     },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.skip_next),
+                    onPressed: _playNextSong,
                   ),
                 ],
               ),
