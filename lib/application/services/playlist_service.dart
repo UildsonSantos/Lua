de04b62ec8c35e985a9f70e52b959a3a9ef10005
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-18 14:14:12 -0300

    refactoring: Update PlaylistService to use  Playlist and Song

--- lib/application/services/playlist_service.dart
+++ lib/application/services/playlist_service.dart
@@ -1,12 +1,11 @@
-import 'package:lua/data/models/models.dart';
+import 'package:lua/domain/entities/entities.dart';
 import 'package:lua/domain/repositories/repositories.dart';
 
 abstract class PlaylistService {
-  Future<void> addSongToPlaylist(PlaylistModel playlist, SongModel song);
-  Future<void> removeSongFromPlaylist(PlaylistModel playlist, SongModel song);
-  Future<void> renamePlaylist(PlaylistModel playlist, String newName);
-  Future<void> reorderPlaylist(
-      PlaylistModel playlist, int oldIndex, int newIndex);
+  Future<void> addSongToPlaylist(Playlist playlist, Song song);
+  Future<void> removeSongFromPlaylist(Playlist playlist, Song song);
+  Future<void> renamePlaylist(Playlist playlist, String newName);
+  Future<void> reorderPlaylist(Playlist playlist, int oldIndex, int newIndex);
 }
 
 class PlaylistServiceImpl implements PlaylistService {
@@ -15,44 +14,39 @@ class PlaylistServiceImpl implements PlaylistService {
   PlaylistServiceImpl(this._repository);
 
   @override
-  Future<void> addSongToPlaylist(PlaylistModel playlist, SongModel song) async {
-    final List<SongModel> updatedSongs =
-        playlist.songs.map((song) => SongModel.fromSong(song)).toList();
+  Future<void> addSongToPlaylist(Playlist playlist, Song song) async {
+    final List<Song> updatedSongs = List.from(playlist.songs);
     updatedSongs.add(song);
     final updatedPlaylist = playlist.copyWith(songs: updatedSongs);
     await _repository.updatePlaylist(updatedPlaylist);
   }
 
   @override
-  Future<void> removeSongFromPlaylist(
-      PlaylistModel playlist, SongModel song) async {
-    final List<SongModel> updatedSongs = playlist.songs
-        .where((s) => s.id != song.id)
-        .map((song) => SongModel.fromSong(song))
-        .toList();
+  Future<void> removeSongFromPlaylist(Playlist playlist, Song song) async {
+    final List<Song> updatedSongs =
+        playlist.songs.where((s) => s.id != song.id).toList();
+
     final updatedPlaylist = playlist.copyWith(songs: updatedSongs);
     await _repository.updatePlaylist(updatedPlaylist);
   }
 
   @override
-  Future<void> renamePlaylist(PlaylistModel playlist, String newName) async {
+  Future<void> renamePlaylist(Playlist playlist, String newName) async {
     final updatedPlaylist = playlist.copyWith(name: newName);
     await _repository.updatePlaylist(updatedPlaylist);
   }
 
   @override
   Future<void> reorderPlaylist(
-      PlaylistModel playlist, int oldIndex, int newIndex) async {
-    final List<SongModel> songs =
-        List.from(playlist.songs.map((song) => SongModel.fromSong(song)));
-    final SongModel song = songs.removeAt(oldIndex);
+      Playlist playlist, int oldIndex, int newIndex) async {
+    final List<Song> songs = List.from(playlist.songs);
+    final Song song = songs.removeAt(oldIndex);
     songs.insert(newIndex, song);
 
     // Ordena a lista de músicas com base na duração
     songs.sort((a, b) => a.duration.compareTo(b.duration));
 
-    final updatedPlaylist =
-        playlist.copyWith(songs: songs.map((songModel) => songModel).toList());
+    final updatedPlaylist = playlist.copyWith(songs: songs);
     await _repository.updatePlaylist(updatedPlaylist);
   }
 }
