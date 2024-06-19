d6d4917de2b0a6dfddd9df2024fded6cec1f84de
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-19 15:34:32 -0300

    feat: Add toggle view button in AppBar and fix horizontal ListView rendering in HomePage

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,39 +1,130 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  bool _isVerticalView = true;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _toggleView() {
+    setState(() {
+      _isVerticalView = !_isVerticalView;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('LUA'),
+        actions: [
+          IconButton(
+            icon: Icon(
+                _isVerticalView ? Icons.list_rounded : Icons.grid_view_rounded),
+            onPressed: _toggleView,
+          ),
+        ],
       ),
-      body: const Column(
+      body: Column(
         children: [
-          ListTile(
+          const ListTile(
             title: Text('Favoritos'),
           ),
-          ListTile(
+          Expanded(
+            child: ListView(
+              scrollDirection:
+                  _isVerticalView ? Axis.horizontal : Axis.vertical,
+              children: [
+                SizedBox(
+                  width: 200.0,
+                  child: ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: const Text('Diretório Favorito 1'),
+                    onTap: () {},
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: const Text('Diretório Favorito 2'),
+                    onTap: () {},
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: const Text('Diretório Favorito 3'),
+                    onTap: () {},
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: const Text('Diretório Favorito 4'),
+                    onTap: () {},
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: const Text('Diretório Favorito 5'),
+                    onTap: () {},
+                  ),
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: ListTile(
+                    leading: const Icon(Icons.folder),
+                    title: const Text('Diretório Favorito 6'),
+                    onTap: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Divider(),
+          const ListTile(
             title: Text('Armazenamento'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.storage),
+            title: const Text('Memória Interna'),
+            onTap: () {},
           ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.folder),
-            label: 'Pasta',
+            label: 'Pastas',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.playlist_play),
-            label: 'Lista de Reprodução',
+            label: 'Playlists',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.more_vert),
             label: 'Mais',
           ),
         ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
