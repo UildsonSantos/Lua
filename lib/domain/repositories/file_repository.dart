ac7343a66acb6aa37232786d07fc460a89e6e8c3
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-25 12:50:28 -0300

    feat: use Either for listFilesAndDirectories in FileRepository and add pagination

--- lib/domain/repositories/file_repository.dart
+++ lib/domain/repositories/file_repository.dart
@@ -1,9 +1,15 @@
 import 'dart:io';
 
+import 'package:dartz/dartz.dart';
+import 'package:lua/core/error/error.dart';
+import 'package:lua/data/models/models.dart';
 import 'package:lua/domain/entities/entities.dart';
 
 abstract class FileRepository {
   Future<List<FileSystemEntity>> listAllFileSystem(Directory directory);
-  Future<DirectoryContents> listFilesAndDirectories(Directory directory);
+  Future<Either<Failure, DirectoryContents>> listFilesAndDirectories(
+      DirectoryModel directory,
+      {int limit = 10,
+      int offset = 0});
   Future<bool> requestPermission();
 }
