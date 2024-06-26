717b5a08df94746a23b1551cc8b8b8047b2b70b4
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-26 12:53:12 -0300

    refactor: update FileRepository methods and remove unused method

--- lib/domain/repositories/file_repository.dart
+++ lib/domain/repositories/file_repository.dart
@@ -1,9 +1,8 @@
 import 'dart:io';
 
-import 'package:lua/domain/entities/entities.dart';
 
 abstract class FileRepository {
-  Future<List<FileSystemEntity>> listAllFileSystem(Directory directory);
-  Future<DirectoryContents> listFilesAndDirectories(Directory directory);
+  Future<Map<String, List<dynamic>>> listDirectoriesAndFiles(String dirPath);
+  Future<Map<String, int>> countFilesAndDirectories(Directory directory);
   Future<bool> requestPermission();
 }
