2f0d8018ef0374e0b9c4c70be474758822f43d0e
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-26 13:12:12 -0300

    refactor: simplify main.dart by removing unused BlocProviders

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:lua/locator.dart' as di;
-import 'package:lua/presentation/blocs/blocs.dart';
 import 'package:lua/presentation/pages/pages.dart';
 
 void main() async {
@@ -16,19 +13,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-      BlocProvider<PlaylistBloc>(
-          create: (_) => GetIt.instance<PlaylistBloc>(),
-        ),
-        BlocProvider<SongBloc>(
-          create: (_) => GetIt.instance<SongBloc>(),
-        ),
-      ],
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
