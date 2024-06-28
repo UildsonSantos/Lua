f8e63954e209ae44847970ebe56f3e9418d89bf1
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-28 15:18:28 -0300

    refactor: Adjust FolderWidget layout to fit content height

--- lib/presentation/widgets/folder_widget.dart
+++ lib/presentation/widgets/folder_widget.dart
@@ -24,108 +24,120 @@ class FolderWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     String title = fileOrDirectory.split('/').last;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 3.5),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(5)),
-          border: isVerticalView! ? Border.all(color: Colors.grey) : null,
-        ),
-        width: 200.0,
-        child: isVerticalView!
-            ? Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+        border: isVerticalView! ? Border.all(color: Colors.grey) : null,
+      ),
+      child: isVerticalView!
+          ? Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 25),
+                  child: Icon(
                     icon,
                     size: 70,
                   ),
-                  ListTile(
-                    title: Text(title),
-                    subtitle: Row(
-                      children: [
-                        if (folderCount > 0) ...[
-                          Text('$folderCount'),
-                          const Icon(Icons.folder_outlined),
-                        ],
-                        const SizedBox(width: 7),
-                        if (fileCount > 0) ...[
-                          Text('$fileCount'),
-                          const Icon(Icons.insert_drive_file_outlined),
-                        ],
+                ),
+                ListTile(
+                  title: Text(title),
+                  subtitle: Row(
+                    children: [
+                      if (folderCount > 0) ...[
+                        Text('$folderCount'),
+                        const Icon(Icons.folder_outlined),
                       ],
-                    ),
-                    trailing: PopupMenuButton(
-                      icon: const Icon(Icons.more_vert),
-                      itemBuilder: (context) => [
-                        const PopupMenuItem(
-                          value: 'favorite',
-                          child: Text('Add to Favorites'),
-                        ),
-                      ],
-                      onSelected: (value) {
-                        if (value == 'favorite' && onAddFavorite != null) {
-                          onAddFavorite!();
-                        }
-                      },
-                    ),
-                    onTap: onTap,
-                  ),
-                ],
-              )
-            : Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(icon, size: 60),
-                  Expanded(
-                    child: ListTile(
-                      title: Text(title),
-                      subtitle: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          if (folderCount > 0) ...[
-                            Text('$folderCount'),
-                            const Icon(Icons.folder_outlined, size: 15.0),
-                          ],
-                          if (fileCount > 0 && folderCount > 0)
-                            const Padding(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 3.0, horizontal: 4.0),
-                              child: Text(
-                                '•',
-                                style: TextStyle(
-                                    fontSize: 10.0,
-                                    height: 0.75,
-                                    color: Colors.grey),
-                              ),
-                            ),
-                          if (fileCount > 0) ...[
-                            Text('$fileCount'),
-                            const Icon(Icons.insert_drive_file_outlined,
-                                size: 13.0),
-                          ],
-                        ],
-                      ),
-                      trailing: PopupMenuButton(
-                        icon: const Icon(Icons.more_vert),
-                        itemBuilder: (context) => [
-                          const PopupMenuItem(
-                            value: 'favorite',
-                            child: Text('Add to Favorites'),
+                      if (fileCount > 0 && folderCount > 0)
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 3.0, horizontal: 4.0),
+                          child: Text(
+                            '•',
+                            style: TextStyle(
+                                fontSize: 10.0,
+                                height: 0.75,
+                                color: Colors.grey),
                           ),
-                        ],
-                        onSelected: (value) {
-                          if (value == 'favorite' && onAddFavorite != null) {
-                            onAddFavorite!();
-                          }
-                        },
-                      ),
-                      onTap: onTap,
-                    ),
+                        ),
+                      if (fileCount > 0) ...[
+                        Text('$fileCount'),
+                        const Icon(Icons.insert_drive_file_outlined),
+                      ],
+                    ],
                   ),
-                ],
+                  trailing: PopupMenuButton(
+                    icon: const Icon(Icons.more_vert),
+                    itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        value: 'favorite',
+                        child: Text('Add to Favorites'),
+                      ),
+                    ],
+                    onSelected: (value) {
+                      if (value == 'favorite' && onAddFavorite != null) {
+                        onAddFavorite!();
+                      }
+                    },
+                  ),
+                  onTap: onTap,
+                ),
+              ],
+            )
+          : Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(icon, size: 60),
+              Expanded(
+                child: ListTile(
+                  title: Text(title),
+                  subtitle: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      if (folderCount > 0) ...[
+                        Text('$folderCount'),
+                        const Icon(Icons.folder_outlined, size: 15.0),
+                      ],
+                      if (fileCount > 0 && folderCount > 0)
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 3.0, horizontal: 4.0),
+                          child: Text(
+                            '•',
+                            style: TextStyle(
+                                fontSize: 10.0,
+                                height: 0.75,
+                                color: Colors.grey),
+                          ),
+                        ),
+                      if (fileCount > 0) ...[
+                        Text('$fileCount'),
+                        const Icon(Icons.insert_drive_file_outlined,
+                            size: 13.0),
+                      ],
+                    ],
+                  ),
+                  trailing: PopupMenuButton(
+                    icon: const Icon(Icons.more_vert),
+                    itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        value: 'favorite',
+                        child: Text('Add to Favorites'),
+                      ),
+                    ],
+                    onSelected: (value) {
+                      if (value == 'favorite' && onAddFavorite != null) {
+                        onAddFavorite!();
+                      }
+                    },
+                  ),
+                  onTap: onTap,
+                ),
               ),
-      ),
+            ],
+          ),
     );
+  
   }
 }
