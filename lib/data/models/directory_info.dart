a50eb66c20641e799626b1267feb7a0bd578f485
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-06-30 20:56:46 -0300

    Refactor DirectoryInfo to include isFavorite field and copyWith method
    - Added a new isFavorite field to the DirectoryInfo class, which is
    initially set to false.
    - Implemented a copyWith method in the DirectoryInfo class, which allows creating a new DirectoryInfo instance with one or
     more fields modified

--- lib/data/models/directory_info.dart
+++ lib/data/models/directory_info.dart
@@ -2,15 +2,31 @@ class DirectoryInfo {
   final String path;
   final int fileCount;
   final int folderCount;
+  bool? isFavorite;
 
   DirectoryInfo({
     required this.path,
     required this.fileCount,
     required this.folderCount,
+    isFavorite = false,
   });
 
   @override
   String toString() {
     return 'DirectoryInfo(path: $path, files: $fileCount, directories: $folderCount)';
+  }
+
+  DirectoryInfo copyWith({
+    String? path,
+    int? fileCount,
+    int? folderCount,
+    bool? isFavorite,
+  }) {
+    return DirectoryInfo(
+      path: path ?? this.path,
+      fileCount: fileCount ?? this.fileCount,
+      folderCount: folderCount ?? this.folderCount,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
   }
 }
