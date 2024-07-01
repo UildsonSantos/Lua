1045e4ef1cc7b2f754dfc9fd32f59fbd4a762cb8
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-07-01 14:27:30 -0300

    feat: Display custom name for directories in  FolderWidget
    - Updated the `FolderWidget` to check if the `DirectoryInfo` has a
     custom `name` field. If it does, the custom name is used as the
     title. Otherwise, the name is extracted from the `path` field as
      before.
    - Adjusted the layout and styling of the `FolderWidget` to
    accommodate the custom name feature.

--- lib/presentation/widgets/folder_widget.dart
+++ lib/presentation/widgets/folder_widget.dart
@@ -21,53 +21,158 @@ class FolderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String title = directoryInfo.path.split('/').last;
+    String title = '';
+    if (directoryInfo.name != null) {
+      title = directoryInfo.name!;
+    } else {
+      title = directoryInfo.path.split('/').last;
+    }
 
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(Radius.circular(5)),
-        border: isVerticalView! ? Border.all(color: Colors.grey) : null,
-      ),
-      child: isVerticalView!
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 25),
-                  child: Icon(
-                    icon,
-                    size: 70,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(5)),
+          border: isVerticalView! ? Border.all(color: Colors.grey) : null,
+        ),
+        child: isVerticalView!
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Icon(
+                      icon,
+                      size: 60,
+                    ),
                   ),
-                ),
-                ListTile(
-                  title: Text(title),
-                  subtitle: Row(
-                    children: [
-                      if (directoryInfo.folderCount > 0) ...[
-                        Text('${directoryInfo.folderCount}'),
-                        const Icon(Icons.folder_outlined),
-                      ],
-                      if (directoryInfo.fileCount > 0 &&
-                          directoryInfo.folderCount > 0)
-                        const Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 3.0, horizontal: 4.0),
-                          child: Text(
-                            '•',
-                            style: TextStyle(
-                                fontSize: 10.0,
-                                height: 0.75,
-                                color: Colors.grey),
-                          ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25.0, left: 15),
+                    child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              title,
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                            Row(
+                              children: [
+                                if (directoryInfo.folderCount > 0) ...[
+                                  Text(
+                                    '${directoryInfo.folderCount}',
+                                  ),
+                                  const Icon(
+                                    Icons.folder_outlined,
+                                    size: 25,
+                                  ),
+                                ],
+                                if (directoryInfo.fileCount > 0 &&
+                                    directoryInfo.folderCount > 0)
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 3.0, horizontal: 4.0),
+                                    child: Text(
+                                      '•',
+                                      style: TextStyle(
+                                          fontSize: 10.0,
+                                          height: 0.75,
+                                          color: Colors.grey),
+                                    ),
+                                  ),
+                                if (directoryInfo.fileCount > 0) ...[
+                                  Text('${directoryInfo.fileCount}'),
+                                  const Icon(
+                                    Icons.insert_drive_file_outlined,
+                                    size: 21,
+                                  ),
+                                ],
+                              ],
+                            ),
+                          ],
                         ),
-                      if (directoryInfo.fileCount > 0) ...[
-                        Text('${directoryInfo.fileCount}'),
-                        const Icon(Icons.insert_drive_file_outlined),
+                        const Spacer(),
+                        BlocBuilder<FavoriteBloc, FavoriteState>(
+                          builder: (context, state) {
+                            bool isFavorite =
+                                state.favorites.contains(directoryInfo);
+                            return PopupMenuButton<String>(
+                              icon: const Icon(Icons.more_vert),
+                              onSelected: (value) {
+                                if (value == 'add') {
+                                  context.read<FavoriteBloc>().add(
+                                      AddFavoriteEvent(folder: directoryInfo));
+                                } else if (value == 'remove') {
+                                  context.read<FavoriteBloc>().add(
+                                      RemoveFavoriteEvent(
+                                          folder: directoryInfo));
+                                }
+                              },
+                              itemBuilder: (context) => [
+                                PopupMenuItem<String>(
+                                  value: isFavorite ? 'remove' : 'add',
+                                  child: Text(isFavorite
+                                      ? 'Remove from Favorites'
+                                      : 'Add to Favorites'),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
                       ],
+                    ),
+                  ),
+                ],
+              )
+            : Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Icon(icon, size: 50),
+                  ),
+                  const SizedBox(width: 14),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(fontSize: 17),
+                      ),
+                      const SizedBox(height: 6),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          if (directoryInfo.folderCount > 0) ...[
+                            Text('${directoryInfo.folderCount}'),
+                            const Icon(Icons.folder_outlined, size: 15.0),
+                          ],
+                          if (directoryInfo.fileCount > 0 &&
+                              directoryInfo.folderCount > 0)
+                            const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 3.0, horizontal: 4.0),
+                              child: Text(
+                                '•',
+                                style: TextStyle(
+                                    fontSize: 10.0,
+                                    height: 0.75,
+                                    color: Colors.grey),
+                              ),
+                            ),
+                          if (directoryInfo.fileCount > 0) ...[
+                            Text('${directoryInfo.fileCount}'),
+                            const Icon(Icons.insert_drive_file_outlined,
+                                size: 13.0),
+                          ],
+                        ],
+                      ),
                     ],
                   ),
-                  trailing: BlocBuilder<FavoriteBloc, FavoriteState>(
+                  const Spacer(),
+                  BlocBuilder<FavoriteBloc, FavoriteState>(
                     builder: (context, state) {
                       bool isFavorite = state.favorites.contains(directoryInfo);
                       return PopupMenuButton<String>(
@@ -93,76 +198,9 @@ class FolderWidget extends StatelessWidget {
                       );
                     },
                   ),
-                  onTap: onTap,
-                ),
-              ],
-            )
-          : Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(icon, size: 60),
-                Expanded(
-                  child: ListTile(
-                    title: Text(title),
-                    subtitle: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        if (directoryInfo.folderCount > 0) ...[
-                          Text('${directoryInfo.folderCount}'),
-                          const Icon(Icons.folder_outlined, size: 15.0),
-                        ],
-                        if (directoryInfo.fileCount > 0 &&
-                            directoryInfo.folderCount > 0)
-                          const Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 3.0, horizontal: 4.0),
-                            child: Text(
-                              '•',
-                              style: TextStyle(
-                                  fontSize: 10.0,
-                                  height: 0.75,
-                                  color: Colors.grey),
-                            ),
-                          ),
-                        if (directoryInfo.fileCount > 0) ...[
-                          Text('${directoryInfo.fileCount}'),
-                          const Icon(Icons.insert_drive_file_outlined,
-                              size: 13.0),
-                        ],
-                      ],
-                    ),
-                    trailing: BlocBuilder<FavoriteBloc, FavoriteState>(
-                      builder: (context, state) {
-                        bool isFavorite =
-                            state.favorites.contains(directoryInfo);
-                        return PopupMenuButton<String>(
-                          icon: const Icon(Icons.more_vert),
-                          onSelected: (value) {
-                            if (value == 'add') {
-                              context
-                                  .read<FavoriteBloc>()
-                                  .add(AddFavoriteEvent(folder: directoryInfo));
-                            } else if (value == 'remove') {
-                              context.read<FavoriteBloc>().add(
-                                  RemoveFavoriteEvent(folder: directoryInfo));
-                            }
-                          },
-                          itemBuilder: (context) => [
-                            PopupMenuItem<String>(
-                              value: isFavorite ? 'remove' : 'add',
-                              child: Text(isFavorite
-                                  ? 'Remove from Favorites'
-                                  : 'Add to Favorites'),
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                    onTap: onTap,
-                  ),
-                ),
-              ],
-            ),
+                ],
+              ),
+      ),
     );
   }
 }
