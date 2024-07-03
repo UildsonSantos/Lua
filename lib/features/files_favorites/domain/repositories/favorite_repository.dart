43301a54926cc46a02713c4d2999d31c745371a8
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-07-03 12:29:54 -0300

    refactor: Simplify addFavorite and removeFavorite parameters in FavoriteRepository class

--- lib/features/files_favorites/domain/repositories/favorite_repository.dart
+++ lib/features/files_favorites/domain/repositories/favorite_repository.dart
@@ -1,8 +1,7 @@
-
 import 'package:lua/shared/data/models/models.dart';
 
 abstract class FavoriteRepository {
-  Future<void> addFavorite(DirectoryInfo directoryInfo);
-  Future<void> removeFavorite(String path);
+  Future<void> addFavorite(String directoryPath);
+  Future<void> removeFavorite(String directoryPath);
   Future<List<DirectoryInfo>> getFavorites();
 }
