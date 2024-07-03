fb737fe26bbf581d6701bd07d327115523f61d8e
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-07-03 12:31:28 -0300

    Refactor: Update FavoriteRepositoryImpl to use FavoritesDAO

--- lib/features/files_favorites/data/repositories/favorite_repository_impl.dart
+++ lib/features/files_favorites/data/repositories/favorite_repository_impl.dart
@@ -1,26 +1,24 @@
-
+import 'package:lua/features/files_favorites/data/dao/dao.dart';
 import 'package:lua/features/files_favorites/domain/repositories/repositories.dart';
 import 'package:lua/shared/data/models/models.dart';
 
 class FavoriteRepositoryImpl implements FavoriteRepository {
+  final FavoritesDAO _favoritesDAO;
+
+  FavoriteRepositoryImpl(this._favoritesDAO);
+
   @override
-  Future<void> addFavorite(DirectoryInfo directoryInfo) {
-    // TODO: implement addFavorite
-    throw UnimplementedError();
+  Future<void> addFavorite(String directoryPath) async {
+    await _favoritesDAO.addToFavorites(directoryPath);
   }
 
   @override
-  Future<List<DirectoryInfo>> getFavorites() {
-    // TODO: implement getFavorites
-    throw UnimplementedError();
+  Future<List<DirectoryInfo>> getFavorites() async {
+    return await _favoritesDAO.getFavoriteDirectories();
   }
 
   @override
-  Future<void> removeFavorite(String path) {
-    // TODO: implement removeFavorite
-    throw UnimplementedError();
+  Future<void> removeFavorite(String directoryPath) async {
+    await _favoritesDAO.removeFromFavorites(directoryPath);
   }
-  
 }
-
-
