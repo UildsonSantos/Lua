5e706e3cfd9a09e191362fbd0473a586e2e01364
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-07-07 23:43:19 -0300

    Refactor FileDAO to use new database schema

--- lib/features/files_explorer/data/dao/file_dao.dart
+++ lib/features/files_explorer/data/dao/file_dao.dart
@@ -1,34 +1,79 @@
-
 import 'package:lua/shared/data/database/database.dart';
-import 'package:lua/shared/data/models/models.dart';
 
 class FileDAO {
-  Future<List<DirectoryInfo>> getAllDirectories() async {
-    final db = await FileDatabase.instance.database;
-    final List<Map<String, dynamic>> maps = await db.query('files');
+  final FileDatabase _database = FileDatabase.instance;
 
-    return List.generate(maps.length, (i) {
-      return DirectoryInfo(
-        path: maps[i]['path'],
-        fileCount: maps[i]['fileCount'],
-        folderCount: maps[i]['folderCount'],
-        isFavorite: maps[i]['isFavorite'] == 1,
-        name: maps[i]['name'],
-      );
-    });
+  Future<void> clearDatabase() async {
+    final db = await _database.database;
+    await db.delete('directories');
   }
 
-  Future<void> saveDirectories(List<DirectoryInfo> directories) async {
-    final db = await FileDatabase.instance.database;
+  Future<int> insertFileOrDirectory(Map<String, dynamic> directory) async {
+    final db = await _database.database;
+    return await db.insert('directories', directory);
+  }
 
-    for (final directory in directories) {
-      await db.insert('files', {
-        'path': directory.path,
-        'fileCount': directory.fileCount,
-        'folderCount': directory.folderCount,
-        'isFavorite': directory.isFavorite ? 1 : 0,
-        'name': directory.name,
-      });
+  Future<void> updateParentId(int id, int parentId) async {
+    final db = await _database.database;
+    await db.update(
+      'directories',
+      {'parent_id': parentId},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<Map<String, List<dynamic>>> getFileOrDirectoryContents(
+      String parentId) async {
+    final db = await _database.database;
+
+// Buscar subdiretórios e arquivos usando o parentId
+    final List<Map<String, dynamic>> directories = await db.query(
+      'directories',
+      where: 'parent_id = ?',
+      whereArgs: [parentId],
+    );
+
+    List<Map<String, dynamic>> subDirectories = directories.map((dir) {
+      return {
+        'id': dir['id'],
+        'path': dir['path'],
+        'type': dir['type'],
+        'parentId': dir['parent_id'],
+        'isFavorite': dir['isFavorite'] == 1 ? true : false,
+        'name': dir['name'],
+      };
+    }).toList();
+
+    List<Map<String, dynamic>> mediaFiles =
+        directories.where((dir) => dir['type'] == 'file').toList();
+
+    // Compute fileCount and folderCount for each subDirectory
+    for (var subDir in subDirectories) {
+      subDir['fileCount'] = await _getFileCount(subDir['path']);
+      subDir['folderCount'] = await _getFolderCount(subDir['path']);
     }
+
+    return {'directories': subDirectories, 'files': mediaFiles};
+  }
+
+  Future<int> _getFileCount(String dirPath) async {
+    final db = await _database.database;
+    final List<Map<String, dynamic>> result = await db.query(
+      'directories',
+      where: 'parent_id = ? AND type = ?',
+      whereArgs: [dirPath, 'file'],
+    );
+    return result.length;
+  }
+
+  Future<int> _getFolderCount(String dirPath) async {
+    final db = await _database.database;
+    final List<Map<String, dynamic>> result = await db.query(
+      'directories',
+      where: 'parent_id = ? AND type = ?',
+      whereArgs: [dirPath, 'dir'],
+    );
+    return result.length;
   }
 }
